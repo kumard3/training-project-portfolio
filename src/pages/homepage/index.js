28bb71e0092f5f7cc39748757e30a8af610ee189
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-07-01 00:08:54 +0530

    HomePage-UI Done

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,10 +1,109 @@
 import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+import PhotoPic from "../../assets/photo.jpeg";
+import SocialData from "../../components/social/socialData";
+import SocialImage from "../../components/social/SocialImage";
+const HomeContainer = styled.div`
+  ${tw`
+  w-full
+  h-full
+    flex
+`};
+`;
+
+const HomeContainerLeft = styled.div`
+  ${tw`
+ flex
+ w-full
+ justify-center
+ items-center
+`};
+`;
+
+const IntroContainer = styled.div`
+  ${tw`
+flex
+flex-col
+ml-48
+`};
+`;
+
+const BigText = styled.h1`
+  ${tw`
+text-white
+text-8xl
+`};
+`;
+
+const MiddleText = styled.h1`
+  ${tw`
+text-middle-text
+text-4xl
+`};
+`;
+const Button = styled.button`
+  border: 2px solid #0fd1fc;
+  border-radius: 8px;
+  ${tw`
+text-white
+w-32
+text-xl
+mt-10
+`};
+`;
+const HomeContainerRight = styled.div`
+  ${tw`
+  flex
+  flex-col
+ justify-center
+ items-center
+  w-full
+
+`};
+`;
+const SocialIconContainer = styled.div`
+  ${tw`
+flex
+justify-center
+ml-96
+
+`};
+`;
+
+const ProfileImageContainer = styled.img`
+  height: 80vh;
+  background-blend-mode: screen;
+`;
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <HomeContainer>
+        <HomeContainerLeft>
+          <IntroContainer>
+            <BigText>I AM DAVID</BigText>
+            <BigText>WELCOME TO MY PORTFOLIO</BigText>
+            <MiddleText>I am a design thinker and a web developer.</MiddleText>
+            <Button>Know more</Button>
+          </IntroContainer>
+        </HomeContainerLeft>
+        <HomeContainerRight>
+          <ProfileImageContainer src={PhotoPic} />
+          <SocialIconContainer>
+            {SocialData.map((social) => {
+              return (
+                <SocialImage
+                  name={social.name}
+                  src={social.src}
+                  logo={social.logo}
+                />
+              );
+            })}
+          </SocialIconContainer>
+        </HomeContainerRight>
+      </HomeContainer>
+    </>
   );
 };
 
